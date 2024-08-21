cd4c26549fba5895bbe7651983877da0a2d338cb
sayangbabang <143243931+sayangbabang@users.noreply.github.com>
2024-08-21 08:23:04 +0700

    cuks

--- latihanjs.js
+++ latihanjs.js
@@ -42,3 +42,50 @@ function displayNumbers() {
     }
 }
 
+// Fungsi greet untuk menyapa seseorang
+function greet(name) {
+    return "Hello, " + name;
+}
+
+let Div = document.getElementById("anjay");
+Div.innerHTML += greet("John") + "<br>";
+Div.innerHTML += greet("Jane") + "<br>";
+Div.innerHTML += greet("Doe") + "<br>";
+
+function calculateSquare(number) {
+    return number * number;
+}
+
+Div.innerHTML += "Kuadrat dari 2: " + calculateSquare(2) + "<br>";
+Div.innerHTML += "Kuadrat dari 5: " + calculateSquare(5) + "<br>";
+Div.innerHTML += "Kuadrat dari 10: " + calculateSquare(10) + "<br>";
+
+const addNumbers = (a, b) => a + b;
+Div.innerHTML += "Penjumlahan 3 + 4: " + addNumbers(3, 4) + "<br>";
+Div.innerHTML += "Penjumlahan 10 + 20: " + addNumbers(10, 20) + "<br> ";
+
+"<br><br>"
+let students = ["Alice", "Bob", "Charlie"];
+students.push("David");
+
+let output = document.getElementById("ntaps");
+students.forEach(function(student) {
+    output.innerHTML += "Nama Siswa: " + student + "<br>";
+});
+
+let car = { brand: "Toyota", model: "Corolla", year: 2020 };
+for (let key in car) {
+    output.innerHTML += key + ": " + car[key] + "<br>";
+}
+let cars = [
+    { brand: "Honda", model: "Civic", year: 2019 },
+    { brand: "Ford", model: "Focus", year: 2018 },
+    { brand: "Nissan", model: "Altima", year: 2021 }
+];
+
+cars.forEach(function(car) {
+    output.innerHTML += "Mobil: " + car.brand + " " + car.model + " (" + car.year + ")<br>";
+});
+
+
+
